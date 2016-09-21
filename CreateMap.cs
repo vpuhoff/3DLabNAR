bbc8108fe1d2385dd1acc653d33546b0752485c0
vpuhoff <vpuhoff@live.ru>
2016-09-21 15:41:09 +1000

    All

---
+++ CreateMap.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreateMap : MonoBehaviour {
+    public float xstart;
+    public float ystart;
+    public float xend;
+    public float yend;
+    public int cellcount;
+    public float cellsize;
+    public Transform cube;
+	// Use this for initialization
+	void Start () {
+        cellsize = xend - xstart;
+        cellsize = cellsize / cellcount;
+        for (float x = xstart; x < xend ; x+= cellsize)
+        {
+            for (float  y = ystart ; y < yend ; y+= cellsize)
+            {
+                if (Random.RandomRange(0,100)>60)
+                {
+                    var cell = Instantiate(cube);
+                    cell.position = new Vector3(x, 2, y);
+                }
+            }
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+}
---
+++ Cube.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cube : MonoBehaviour {
+    Vector3 old;
+    // Use this for initialization
+    float f;
+	void Start () {
+        old = this.transform.position;
+        f = Random.RandomRange(0, 10);
+    }
+    float d = 0;
+	// Update is called once per frame
+	void Update () {
+        if (Random.RandomRange(0, 100) > 98)
+        {
+            this.transform.position = new Vector3(0, 0, 0);
+        }
+        else
+        {
+            d += Time.deltaTime;
+            if (d>f)
+            {
+                if (this.transform.position != old)
+                {
+                    this.transform.position = old;
+                }
+                d = 0;
+            }
+            
+        }
+    }
+}
